bd3b449ef3bb54f8930a4f4a584ef4234cf36f1f
LiuLi <li.liu@uipath.com>
2021-07-30 17:41:35 +0900

    bug fix and secure issue and refactoring code

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bcrypt =require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
     client:'pg',
@@ -23,96 +27,11 @@ app.get('/',(req,res)=>{
     .then(user => res.json(user));
 })
 
-app.post('/signin',(req,res)=>{
-    db.select('email','hash').from('login')
-    .where('email','=',req.body.email)
-    .then(data =>{
-        const isValid = bcrypt.compareSync(req.body.password,data[0].hash);
-        if(isValid){
-            return db.select('*').from('users')
-            .where('email','=',req.body.email)
-            .then(user => {
-                res.json(user[0]);
-            })
-            .catch(err => res.status(400).json('unable to get user'))
-        }else{
-            res.status(400).json('wrong credentials');
-        }
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
-})
-
-app.post('/register',(req,res)=>{
-    const {email,name,password} = req.body;
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx =>{
-        trx.insert({
-            hash:hash,
-            email:email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail =>{
-            return trx('users')
-            .returning('*')
-            .insert({
-                email:loginEmail[0],
-                name:name,
-                joined: new Date()
-            })
-            .then(user =>{
-                res.json(user[0]);
-            })
-            .catch(err => res.status(400).json('unable to register.'));
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-})
-
-app.get('/profile/:id',(req,res) => {
-    const {id} = req.params;
-    db.select('*').from('users').where({id})
-    .then(user =>{
-        if(user.length){
-            res.json(user[0]);
-        }else{
-            res.status(400).json('error getting user');
-        }  
-    })
-})
-
-
-app.put('/image',(req,res) =>{
-    const {id} = req.body;
-    db('users').where('id','=',id)
-    .increment('entries',1)
-    .returning('entries')
-    .then(entries =>{
-        res.json(entries[0])
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
-})
-
+app.post('/signin',signin.handleSignin(db,bcrypt)) //chain parameter which is an adavanced way, req and res will be passed automatically
+app.post('/register',(req,res) => {register.handleRegister(req,res,db, bcrypt)})
+app.get('/profile/:id',(req,res) => { profile.handleProfileGet(req,res,db)})
+app.put('/image',(req,res) =>{image.handleImage(req,res,db)})
+app.post('/imageurl',(req,res) =>{image.handleApiCall(req,res)})
 app.listen(3000,()=>{
     console.log('app is running on port 3000');
 })
-
-
-
-
-// // Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//     // res = false
-// });
-
-/*
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST =new user
-/profile/:userId --> GET = user info
-/image --> PUT  --> user
-*/
